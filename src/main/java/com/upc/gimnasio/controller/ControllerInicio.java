7a58ed82fa58b2e2ef8458ba382ab2e95ba73c10
KaosSR <yordansalvadorromani@gmail.com>
2023-02-25 20:22:38 -0500

    first commit

---
+++ src/main/java/com/upc/gimnasio/controller/ControllerInicio.java
@@ -0,0 +1,32 @@
+package com.upc.gimnasio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ControllerInicio {
+
+    @GetMapping({"/","/login"})
+    public String login(){
+     return "login";
+    }
+    @GetMapping("menu")
+    public String menu(){
+        return "menu";
+    }
+    @GetMapping("registroU")
+    public String registroU(){
+        return "registro_usuario";
+    }
+    @GetMapping("registroMA")
+    public String registroMA(){
+        return "medicion";
+    }
+    @GetMapping("definicionPlan")
+    public String definicionPlan(){
+        return "definicion_plan";
+    }
+
+}
